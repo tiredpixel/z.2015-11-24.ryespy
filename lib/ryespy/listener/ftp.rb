509f05daf02fcf794a8089787d23e01f3f5bab12
Marco Sehrer <ms@ninjaconcept.com>
2015-11-24 10:11:02 +0100

    FTP listener - allow to traverse sub directories - catch possible encoding errors

--- lib/ryespy/listener/ftp.rb
+++ lib/ryespy/listener/ftp.rb
@@ -6,10 +6,10 @@ require_relative 'base'
 module Ryespy
   module Listener
     class FTP < Base
-      
+
       REDIS_KEY_PREFIX  = 'ftp'.freeze
       SIDEKIQ_JOB_CLASS = 'RyespyFTPJob'.freeze
-      
+
       def initialize(opts = {})
         @ftp_config = {
           :host     => opts[:host],
@@ -18,46 +18,53 @@ module Ryespy
           :username => opts[:username],
           :password => opts[:password],
         }
-        
+
         super(opts)
       end
-      
+
       def close
         @ftp.close
       end
-      
+
       def check(dir)
         @logger.debug { "dir: #{dir}" }
-        
+
         @logger.debug { "redis_key: #{redis_key(dir)}" }
-        
+
         seen_files = @redis.hgetall(redis_key(dir))
-        
+
         unseen_files = get_unseen_files(dir, seen_files)
-        
+
         @logger.debug { "unseen_files: #{unseen_files}" }
-        
-        unseen_files.each do |filename, checksum|
-          @redis.hset(redis_key(dir), filename, checksum)
-          
-          @notifiers.each { |n| n.notify(SIDEKIQ_JOB_CLASS, [dir, filename]) }
+
+        unseen_files.each do |key, file_info|
+          notifier_args = [file_info[:dir], file_info[:filename]]
+          begin
+            notifier_args.to_json #try upfront to convert to json
+            @redis.hset(redis_key(dir), key, file_info[:checksum])
+            @notifiers.each do |n|
+              n.notify(SIDEKIQ_JOB_CLASS, notifier_args)
+            end
+          rescue Encoding::UndefinedConversionError => e
+            @logger.debug { "Error processing #{file_info.inspect} (#{e})"}
+          end
         end
-        
+
         @logger.info { "#{dir} has #{unseen_files.count} new files" }
       end
-      
+
       private
-      
+
       def connect_service
         @ftp = Net::FTP.new
-        
+
         @ftp.connect(@ftp_config[:host], @ftp_config[:port])
-        
+
         @ftp.passive = @ftp_config[:passive]
-        
+
         @ftp.login(@ftp_config[:username], @ftp_config[:password])
       end
-      
+
       def redis_key(dir)
         [
           REDIS_KEY_PREFIX,
@@ -67,28 +74,43 @@ module Ryespy
           dir,
         ].join(':')
       end
-      
+
       def get_unseen_files(dir, seen_files)
         @ftp.chdir(dir)
-        
+
         files = {}
-        
+
         @ftp.nlst.each do |file|
-          mtime = @ftp.mtime(file).to_i
-          size = @ftp.size(file) rescue nil # ignore non-file error
-          
+          size = @ftp.size(file) rescue nil #ignore non-file error
+
           if size # exclude directories
+            mtime = @ftp.mtime(file).to_i
             checksum = "#{mtime},#{size}".freeze
-            
-            if seen_files[file] != checksum
-              files[file] = checksum
+            key = Digest::MD5.hexdigest("#{dir}/#{file}")
+
+            if seen_files[key] != checksum
+              files[key] = {
+                checksum: checksum,
+                filename: file,
+                dir: dir
+              }
+            end
+          else # check subdirectory
+            begin
+              _dir = "#{dir}/#{file}"
+              unless @ftp.nlst(file).empty?
+                _files = get_unseen_files(_dir, seen_files)
+                files.merge!(_files)
+                @ftp.chdir(dir)
+              end
+            rescue Net::FTPError => ex
+              @logger.error { "Cannot travserse directory '#{_dir}' (#{ex})" }
             end
           end
         end
-        
         files
       end
-      
+
     end
   end
 end
